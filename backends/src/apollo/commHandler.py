4d39f0ab17bde13ef7efe9e2b2c6160c1bd0a2c2
rsyuxiangyang <yuxiangyang@rongshangsoft.com>
2015-02-06 13:23:30 +0800

    backends为apollo后台架构程序版本

---
+++ backends/src/apollo/commHandler.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+#-*-coding:utf-8-*- 
+import dbus
+import sys
+import json
+from common.utils.log import log4py
+# 该接口为测试环境
+#SERVICE_NAME='com.testModule.frontServer'
+#OBJECT_PATH='/com/testModule/frontServer/object'
+#INTERFACE='com.testModule.frontServerInterface'
+# 该接口为生产环境
+SERVICE_NAME='com.bmjc.ui'
+OBJECT_PATH='/bmjc/ui'
+INTERFACE='bmjc.ui'
+def handleMsgFromUI():    
+    arguments=sys.argv
+    temp=arguments[1]
+    temp=json.loads(temp)
+    msgToUI={}
+    msgToUI["scenename"]=str(temp["scenename"])
+    msgToUI["functionname"]=str(temp["functionname"])
+    return msgToUI
+class CommHandler:
+    def __init__(self):
+        pass   
+        
+    def orgDataReportMsg(self,dataMsgPara):
+        dataReportMsg=handleMsgFromUI()
+        dataReportMsg["resulttype"]="datareport"
+        dataReportMsg["result"]=dataMsgPara  
+        return json.dumps(dataReportMsg)
+    
+    def orgErrReportMsg(self,errordescritionPara):
+        errReportMsg=handleMsgFromUI()
+        errReportMsg["resulttype"]="errorreport"
+        resultTemp={}
+        resultTemp["errordescrition"]=str(errordescritionPara)
+        errReportMsg["result"]=resultTemp  
+        return json.dumps(errReportMsg)
+    
+    def orgProgReportMsg(self,currentcompletionPara,currentstatusPara):
+        progReportMsg=handleMsgFromUI()
+        progReportMsg["resulttype"]="progressreport"
+        resultTemp={}
+        resultTemp["currentcompletion"]=str(currentcompletionPara)
+        resultTemp["currentstatus"]=str(currentstatusPara)
+        progReportMsg["result"]=resultTemp  
+        return json.dumps(progReportMsg)
+    
+    def sendMsgToUI(self, resultMsgPara):        
+        bus =dbus.SessionBus()
+        obj = bus.get_object(SERVICE_NAME,OBJECT_PATH)
+        iface =dbus.Interface(obj,INTERFACE)  
+        log4py.info("backends向UI发送信息:"+str(resultMsgPara))  
+        responseMsgFromUI = iface.updateFromTool(resultMsgPara)
+        log4py.info("backends收到UI响应信息:"+str(responseMsgFromUI))      
---
+++ backends/src/common/enums/enumReportType.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*- 
+class EnumReportType:
+    reportType={
+                0:"datareport",
+                1:"errorreport",
+                2:"progressreport"}
+    errReportResult={
+                       0:"errordescrition"}
+    progReportResult={
+                      0:"currentcompletion",
+                      1:"currentstatus"}      
