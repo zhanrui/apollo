be21a5723862530aea1ed0cc8cb2281b4d111cdf
yueshi126 <yue_shi126@126.com>
2015-03-17 09:25:22 +0800

    网络设备

--- backends/src/apollo/deviceConnectInfo/netConfig.py
+++ backends/src/apollo/deviceConnectInfo/netConfig.py
@@ -21,18 +21,37 @@ class NetConfig(commHandler.CommHandler):
         pass 
 
     def getNetConfig(self):
-        net = {}       
-        n = os.popen('ifconfig -a')
-        network = n.read()
-        n.close()
-        if network :
-            if(re.findall('inet',network)):
-                networkList = network[network.index('inet'):].replace(":"," ").replace("\n","").split(" ")
-                net['IpAddr'] = networkList[1]
-                net['netmask'] =networkList[4] 
-                net['broadcast'] =networkList[7] 
-
-        return net
+        netdic = {}   
+        netlist = []
+        order = "cat /proc/net/dev | awk \'{if($2>0 && NR > 2) print substr($1, 0, index($1, \":\") - 1)}\'"    
+        netname =  os.popen(order)  
+        netnamecon = netname.readlines()
+        netname.close()
+        for readline in netnamecon:
+            netname = readline.replace('\n','')
+            netemp = os.popen('ifconfig '+netname)
+            netcontemp = netemp.read()
+            if netcontemp :
+                if(re.findall('inet',netcontemp)):
+                    networkList = netcontemp[netcontemp.index('inet'):].replace(":"," ").replace("\n","").split(" ")
+                    netdic['netname'] = netname
+                    netdic['IpAddr'] = networkList[1]
+                    netdic['netmask'] =networkList[4]   # 子网掩码
+#                     netdic['broadcast'] =networkList[7] 
+                    macaddr = ''
+                    if re.findall('HWaddr(.*)',netcontemp):
+                        macaddr = re.findall('HWaddr(.*)',netcontemp)
+                    netdic['macaddr'] =macaddr
+                    netlist.append(netdic)
+        dnsorder = 'cat /etc/resolv.conf | grep nameserver | awk \'{print $2}\''
+        dnsopen = os.popen(dnsorder)
+        dnsread = dnsopen.readlines()
+        dnsopen.close()
+        dns = ''
+        for readline in dnsread:
+            dns = readline.replace('\n','')+'/'+ dns  
+        netdic['dns'] =dns
+        return netlist
 
 
 if __name__ == "__main__":    
@@ -44,5 +63,5 @@ if __name__ == "__main__":
         temp.sendMsgToUI(temp.orgProgReportMsg("100", "网络配置检查完毕."))
     except Exception as e:
         #print e
-        log4py.error("检查网络配置出错."  )
+        log4py.error("检查网络配置出错."  ,str(e))
         temp.sendMsgToUI(temp.orgErrReportMsg("网络配置检查出错."))  
